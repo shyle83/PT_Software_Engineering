f92434204fc44f28355db20510589ebd4e4d6233
PSCBUS_1015 <pscbus_1015@PSCBUS-1015.local>
2020-06-04 21:33:52 -0400

    Did week 1day3 lab

---
+++ Mod1/week-1/w1d3/student_labw1d3.js
@@ -0,0 +1,54 @@
+//Loop It!
+
+
+for (let i = 0; i < 15; i++) {
+    console.log('hi');
+}
+
+for (let i = 15; i <= 45; i++) {
+    console.log('YEP');
+}
+
+//Practice Below
+
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+
+//conditional - "If the code below it 'true' it will execute 'x' times
+
+x = 0; {
+
+if (x < 10) {
+console.log('Get Motivated');
+}
+
+}
+
+//The purpose of a while loop is to repeat a 'x' amount of times until you reach your exit condition 
+
+//x = 0;
+
+//while (x < run) {                confused on while loops and I always create an infinite loop!!
+ //   x = x + 10;
+//}
+
+//Booleans & Loops - Part 1
+
+for (let x = 0; x <=10; i++) {
+
+    console.log('dunk');
+}
