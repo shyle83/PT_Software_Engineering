fa9d282211be2ea98f9ac40f9cba16ba38a3d00a
PSCBUS_1015 <pscbus_1015@PSCBUS-1015.local>
2020-06-06 15:09:15 -0400

    Finished home up to Ninja Turtles

---
+++ Mod1/week-2/w2d2/homework/answers.js
@@ -0,0 +1,72 @@
+//Part II
+
+for (let i = 0; i <201; i += 2) {
+    console.log.(i)
+}
+
+//if i is divisible by 2 with a remainder  of 0
+
+//Fizz Buzz
+//#1
+for (let i = 0; i <= 100; i++) {
+    console.log('Hi', i);
+}
+
+//#2
+ for (let i = 0; i <= 100; i++) {
+     if(i % 3 === 0) {
+      console.log(Fizz, i);
+ }
+
+//#3 
+for (let i = 0; i <= 100; i++) {
+    if(i % 5 === 0)
+    console.log(buzz);
+}
+
+//#4
+
+for (let i = 0; <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0)
+    console.log(fizzBuzz);
+}
+
+//WildLife
+//#1
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+console.log(plantee[2] +1)
+
+//#2
+plantee.splice(2,1), 5000 + 1)
+console.log(plantee)
+
+for(let i = 0; i < plantee.length; i++) {
+    if(plantee[i] === 5000) {
+        console.log(plantee[1] +1)
+        console.loge(plantee)
+    }
+}
+
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+
+wolfy[3] = 'Gotham City'
+
+console.log(wolfy)
+
+//#3
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+dart.splice(3,0,'Hawkins')
+console.log(dar)
+
+//#4
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+
+wolfy.splice(0, 1, 'Gameboy')
+console.log(wolfy)
+
+//or
+
+wolfy[0] = 'gameboy'
+console.log(wolfy)
